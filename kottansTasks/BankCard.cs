ee49c75e96663fc124c6bfa9bbbfaf4e7548da27
Andriy1236 <v4r4@yandex.ru>
2016-05-06 14:06:23 +0300

    Update BankCard.cs

--- kottansTasks/BankCard.cs
+++ kottansTasks/BankCard.cs
@@ -1,50 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace kottansTasks
 {
-   public static class BankCard
+  public static class BankCard
     {
         public static string GetCreditCardVendor(string cardNumber)
         {
-            cardNumber = ConvertToStringWithoutSpace(cardNumber);
-            string cardVendor = "Unknown";
-            if (IsCreditCardNumberValid(cardNumber))
+        cardNumber = ConvertToStringWithoutSpace(cardNumber);
+        string cardVendor = "Unknown";
+        if (IsCreditCardNumberValid(cardNumber))
+        {
+            int cardNumberLength = cardNumber.Length;
+            int firstTwoNumbers = Convert.ToInt32(cardNumber.Substring(0, 2));
+            int firstFourNumbers = Convert.ToInt32(cardNumber.Substring(0, 4));
+            int firstNumber = Convert.ToInt32(cardNumber.Substring(0, 1));
+
+            if (cardNumberLength == 15 && ((firstTwoNumbers == 34) || (firstTwoNumbers == 37)))
             {
-                int cardNumberLength = cardNumber.Length;
-                int firstTwoNumbers = Convert.ToInt32(cardNumber.Substring(0, 2));
-                int firstFourNumbers = Convert.ToInt32(cardNumber.Substring(0, 4));
-                int firstNumber = Convert.ToInt32(cardNumber.Substring(0, 1));
-
-                if (cardNumberLength == 15 && ((firstTwoNumbers == 34) || (firstTwoNumbers == 37)))
-                {
-                    return "American Express";
-                }
-
-                if ((12 <= cardNumberLength && cardNumberLength <= 19) &&
-                    (50 == firstTwoNumbers || ((56 <= firstTwoNumbers) && (firstTwoNumbers >= 59))))
-                {
-                    cardVendor = "Maestro";
-                }
-
-                if (cardNumberLength == 16 && (51 <= firstTwoNumbers && firstTwoNumbers <= 55))
-                {
-                    cardVendor = "MasterCard";
-                }
-
-                if ((cardNumberLength == 13 || cardNumberLength == 16 || cardNumberLength == 19) && firstNumber == 4)
-                {
-                    cardVendor = "Visa";
-                }
-
-                if (cardNumberLength == 16 && (3528 <= firstFourNumbers && firstFourNumbers <= 3589))
-                {
-                    cardVendor = "JCB";
-                }
+                return "American Express";
             }
-            return cardVendor;
-            
+
+            if ((12 <= cardNumberLength && cardNumberLength <= 19) &&
+                (50 == firstTwoNumbers || ((56 <= firstTwoNumbers) && (firstTwoNumbers >= 59))))
+            {
+                cardVendor = "Maestro";
+            }
+
+            if (cardNumberLength == 16 && (51 <= firstTwoNumbers && firstTwoNumbers <= 55))
+            {
+                cardVendor = "MasterCard";
+            }
+
+            if ((cardNumberLength == 13 || cardNumberLength == 16 || cardNumberLength == 19) && firstNumber == 4)
+            {
+                cardVendor = "Visa";
+            }
+
+            if (cardNumberLength == 16 && (3528 <= firstFourNumbers && firstFourNumbers <= 3589))
+            {
+                cardVendor = "JCB";
+            }
+        }
+        return cardVendor;
         }
 
         public static bool IsCreditCardNumberValid(string cardNumber)
@@ -59,24 +58,23 @@ namespace kottansTasks
                 throw new Exception("card number has not enough numbers");
             }
             List<int> numericalСardNumbers = new List<int>();
-                numericalСardNumbers.AddRange(cardNumber.Reverse().Select(x => int.Parse(x.ToString()))); 
-                for (int i = 0; i < numericalСardNumbers.Count; i++)
+            numericalСardNumbers.AddRange(cardNumber.Reverse().Select(x => int.Parse(x.ToString()))); 
+            for (int i = 0; i < numericalСardNumbers.Count; i++)
+            {
+                if (i%2 != 0) 
                 {
-                    if (i%2 != 0) 
+                    numericalСardNumbers[i] = numericalСardNumbers[i]*2;
+                    if (numericalСardNumbers[i] > 9)
                     {
-                        numericalСardNumbers[i] = numericalСardNumbers[i]*2;
-                        if (numericalСardNumbers[i] > 9)
-                        {
-                            numericalСardNumbers[i] = numericalСardNumbers[i]/10 + numericalСardNumbers[i]%10;
-                        }
+                        numericalСardNumbers[i] = numericalСardNumbers[i]/10 + numericalСardNumbers[i]%10;
                     }
                 }
-            return numericalСardNumbers.Sum(x => x) % 10 == 0;
+            }
+        return numericalСardNumbers.Sum(x => x) % 10 == 0;
         }
 
         public static string GenerateNextCreditCardNumber(string cardNumber)
         {
-          
             if (IsCreditCardNumberValid(cardNumber))
             {
                 string cardVendor = GetCreditCardVendor(cardNumber);
@@ -88,7 +86,6 @@ namespace kottansTasks
                     cardNumber = integerСardnumber.ToString();
                     if (IsCreditCardNumberValid(cardNumber) && GetCreditCardVendor(cardNumber) == cardVendor)
                     {
-                       
                         return cardNumber;
                     }
                     else
@@ -96,14 +93,13 @@ namespace kottansTasks
                         throw new Exception("no more card numbers available for this vendor");
                     }
                 }
+            }
+            return cardNumber;
         }
-
         private static string ConvertToStringWithoutSpace(string cardNumber)
         {
            return cardNumber.Replace(" ", "");
         }
-
-
     }
 }
 
